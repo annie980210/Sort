63f121aef48e54351eec5400ffab44c552f20fdb
annie980210 <40270263+annie980210@users.noreply.github.com>
2018-06-14 23:14:33 +0900

    Create SA.c

---
+++ SA.c
@@ -0,0 +1,93 @@
+#include "SA.h"
+
+/*
+void insertionSort(DT * arr, int length)
+{
+	int i, j;
+	DT temp;
+	for (i = 1; i < length; i++)
+	{
+		temp = arr[i];					//삽입할 대상을
+		j = i - 1;						//뒤에서부터
+		while (j >= 0 && arr[j] > temp) //밀어내면서 적절한 자리를 찾으면
+		{
+			arr[j + 1] = arr[j];
+			j -= 1;
+		}
+		arr[j + 1] = temp;				//그곳에 삽입
+	}
+}
+*/
+void insertionSortRec(Rectangles * arr, int length)
+{
+	int i, j;
+	Rectangles temp;
+	for (i = 1; i < length; i++)
+	{
+		temp = arr[i];
+		j = i - 1;
+		while (j >= 0 && arr[j].height * arr[j].weight > temp.height * temp.weight)
+		{
+			arr[j + 1] = arr[j];
+			j -= 1;
+		}
+		arr[j + 1] = temp;
+	}
+}
+
+/*
+int middleValue3(DT * arr, int one, int two, int three)
+{
+	if (arr[one] < arr[two])
+	{
+		if (arr[two] < arr[three])
+			return two;
+		else if (arr[three] < arr[one])
+			return one;
+		else
+			return three;
+	}
+	else if (arr[one] > arr[two])
+	{
+		if (arr[one] < arr[three])
+			return one;
+		else if (arr[three] < arr[two])
+			return two;
+		else
+			return three;
+	}
+	else
+		return one;
+}
+
+
+void quickSort(DT * arr, int left, int right)
+{
+	int i, j;
+	int pivotIndex = middleValue3(arr, left, (left + right) / 2, right);
+	DT pivot = arr[pivotIndex];
+	
+	if (pivotIndex == left)
+		i = left + 1, j = right;
+	else if (pivotIndex == right)
+		i = left, j = right + 1;
+	else
+		i = left, j = right;
+
+	while (arr[i] < pivot)
+		i++;
+	while (pivot < arr[j])
+		j--;
+
+
+
+
+
+
+}
+void quickSortRec(Rectangles * arr, int left, int right);
+void iterativeMergeSort(DT * arr, int length);
+void iterativeMergeSortRec(Rectangles * arr, int length);
+void heapSort(DT * arr, int length);
+void heapSortRec(Rectangles * arr, int length);
+*/
