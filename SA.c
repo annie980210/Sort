0f6555635aaca5666ff5de69328dc75f3a841cae
annie980210 <40270263+annie980210@users.noreply.github.com>
2018-06-15 16:10:55 +0900

    Update SA.c

--- SA.c
+++ SA.c
@@ -85,6 +85,41 @@ void quickSort(DT * arr, int left, int right)
 
 
 }
+
+void quickSort(DT *arr, int left, int right)
+{
+        int i, j;
+        DT temp;
+        DT pivot = arr[left];
+        if(left < right)
+        {
+               i = left;
+               j = right+1;
+               while( i <= j)
+               {
+                       do
+                       i++;
+                       while(arr[i] > pivot);
+                       do
+                       j--;
+                       while(arr[j] < pivot);
+                       if(i<j)
+                       {
+                              temp = arr[i];
+                              arr[i] = arr[j];
+                              arr[j] = temp;
+                       }
+                       else
+                             break;
+               }
+               temp = arr[j];
+               arr[j] = arr[left];
+               arr[left] = temp;
+               quickSort(arr, left, j-1);
+               quickSort(arr, j+1, right);
+        }
+}
+
 void quickSortRec(Rectangles * arr, int left, int right);
 void iterativeMergeSort(DT * arr, int length);
 void iterativeMergeSortRec(Rectangles * arr, int length);
